60f75b63dfdbbab8a144543f73013fd6181feb8d
sergioTFG <pksergioest@gmail.com>
2024-05-30 14:06:49 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,3 +4,5 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     completed = db.Column(db.Boolean, default=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=True)
